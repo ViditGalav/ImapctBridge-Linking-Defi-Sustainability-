b343554e51739b1a0f57ef52e4dc8c456e13b593
VishalVermaa01 <vermavishal6020@gmail.com>
2023-09-24 19:51:28 +0530

    Added field for IPFS hash

--- investor_Registration.sol
+++ investor_Registration.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier:MIT
 pragma solidity ^0.8.0;
 
 import "./tokenCreation.sol"; 
@@ -10,12 +10,21 @@ contract InvestorRegistry {
     struct Investor {
         string name;
         string email;
+        string nationality;
         bool registered;
+        string ipfsFinancialDocumentHash;
     }
 
     mapping(address => Investor) public investors;
 
-    event InvestorRegistered(address indexed investor, string name, string email);
+    event InvestorRegistered(
+        address indexed investor,
+        string indexed name,
+        string indexed email,
+        string nationality,
+        bool registered,
+        string ipfsFinancialDocumentHash
+    );
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can call this function");
@@ -24,19 +33,34 @@ contract InvestorRegistry {
 
     constructor(address _tokenAddress) {
         owner = msg.sender;
-        impactToken = ImpactToken(_tokenAddress); // Set the Impact Token contract address
+        impactToken = ImpactToken(_tokenAddress); // Setting the Impact Token contract address
     }
 
-    function register(string memory name, string memory email) public {
+    function register(
+        string memory name,
+        string memory email,
+        string memory nationality,
+        string memory ipfsDocumentHash
+    ) public {
         require(!investors[msg.sender].registered, "Investor already registered");
+        require(bytes(ipfsDocumentHash).length > 0, "IPFS document hash cannot be empty");
         require(impactToken.balanceOf(msg.sender) > 0, "You must own Impact Tokens to register");
 
         investors[msg.sender] = Investor({
             name: name,
             email: email,
-            registered: true
+            nationality: nationality,
+            registered: true,
+            ipfsFinancialDocumentHash: ipfsDocumentHash 
         });
 
-        emit InvestorRegistered(msg.sender, name, email);
+        emit InvestorRegistered(
+            msg.sender,
+            name,
+            email,
+            nationality,
+            true,
+            ipfsDocumentHash
+        );
     }
 }
