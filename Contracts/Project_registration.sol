83b49b29060b9f252a5183b8d0af6a4d10d04d65
Aditya Singh <124086655+adityaIIITR2062@users.noreply.github.com>
2023-09-26 01:16:15 +0530

    Project registration with ipfs integration

--- Contracts/Project_registration.sol
+++ Contracts/Project_registration.sol
@@ -2,54 +2,108 @@
 pragma solidity ^0.8.0;
 
 contract ProjectRegistry {
-    // Struct to represent a project
     struct Project {
         string name;
         string techUsed;
         uint256 fundsRequired;
+        string socialDocumentIPFSHash;
+        string environmentalDocumentIPFSHash;
+        string financeDocumentIPFSHash;
     }
 
-    // Mapping to store projects by their unique project ID
     mapping(uint256 => Project) public projects;
-
-    // Counter to keep track of the number of registered projects
     uint256 public projectCount;
 
-    // Event to log project registration
-    event ProjectRegistered(uint256 projectId, string name, string techUsed, uint256 fundsRequired);
+    event ProjectRegistered(
+        uint256 projectId,
+        string name,
+        string techUsed,
+        uint256 fundsRequired,
+        string socialDocumentIPFSHash,
+        string environmentalDocumentIPFSHash,
+        string financeDocumentIPFSHash
+    );
 
-    // Function to register a new project
-    function registerProject(string memory _name, string memory _techUsed, uint256 _fundsRequired) public {
+    function registerProject(
+        string memory _name,
+        string memory _techUsed,
+        uint256 _fundsRequired,
+        string memory _socialDocumentIPFSHash,
+        string memory _environmentalDocumentIPFSHash,
+        string memory _financeDocumentIPFSHash
+    ) public {
         require(bytes(_name).length > 0, "Project name cannot be empty");
         require(bytes(_techUsed).length > 0, "Technology used cannot be empty");
         require(_fundsRequired > 0, "Funds required must be greater than 0");
 
-        // Increment the project count to generate a unique project ID
         projectCount++;
 
-        // Store the project details in the mapping
-        projects[projectCount] = Project(_name, _techUsed, _fundsRequired);
+        projects[projectCount] = Project(
+            _name,
+            _techUsed,
+            _fundsRequired,
+            _socialDocumentIPFSHash,
+            _environmentalDocumentIPFSHash,
+            _financeDocumentIPFSHash
+        );
 
-        // Emit an event to log the project registration
-        emit ProjectRegistered(projectCount, _name, _techUsed, _fundsRequired);
+        emit ProjectRegistered(
+            projectCount,
+            _name,
+            _techUsed,
+            _fundsRequired,
+            _socialDocumentIPFSHash,
+            _environmentalDocumentIPFSHash,
+            _financeDocumentIPFSHash
+        );
     }
 
-    // Function to get project details by project ID
-    function getProjectDetails(uint256 projectId) public view returns (string memory, string memory, uint256) {
+    function getProjectDetails(uint256 projectId)
+        public
+        view
+        returns (
+            string memory,
+            string memory,
+            uint256,
+            string memory,
+            string memory,
+            string memory
+        )
+    {
         require(projectId > 0 && projectId <= projectCount, "Invalid project ID");
-        
+
         Project storage project = projects[projectId];
-        return (project.name, project.techUsed, project.fundsRequired);
+        return (
+            project.name,
+            project.techUsed,
+            project.fundsRequired,
+            project.socialDocumentIPFSHash,
+            project.environmentalDocumentIPFSHash,
+            project.financeDocumentIPFSHash
+        );
     }
 
-    // Function to update project details by project ID
-    function updateProjectDetails(uint256 projectId, string memory _name, string memory _techUsed, uint256 _fundsRequired) public {
+    function updateProjectDetails(
+        uint256 projectId,
+        string memory _name,
+        string memory _techUsed,
+        uint256 _fundsRequired,
+        string memory _socialDocumentIPFSHash,
+        string memory _environmentalDocumentIPFSHash,
+        string memory _financeDocumentIPFSHash
+    ) public {
         require(projectId > 0 && projectId <= projectCount, "Invalid project ID");
         require(bytes(_name).length > 0, "Project name cannot be empty");
         require(bytes(_techUsed).length > 0, "Technology used cannot be empty");
         require(_fundsRequired > 0, "Funds required must be greater than 0");
 
-        // Update the project details in the mapping
-        projects[projectId] = Project(_name, _techUsed, _fundsRequired);
+        projects[projectId] = Project(
+            _name,
+            _techUsed,
+            _fundsRequired,
+            _socialDocumentIPFSHash,
+            _environmentalDocumentIPFSHash,
+            _financeDocumentIPFSHash
+        );
     }
 }
