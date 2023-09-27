80d154cfe842858f6cd0e7202f5be4fe20e1a763
komanbibek <141799962+komanbibek@users.noreply.github.com>
2023-09-27 18:13:32 +0530

    Add files via upload

---
+++ InvestorRetursn.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+contract InvestmentReturn{
+
+
+  
+
+    function returnOnInvestment(uint investmentPoolAmount, uint InvestorContribution, uint FundsAllocated) public pure returns(uint) {
+        require(investmentPoolAmount>0,"Investment pool amount must be greatr than zero");
+        uint256 contributionPercentage= (InvestorContribution*100)/investmentPoolAmount;
+        require(contributionPercentage>0,"ContributionPercentage can't be zero");
+        uint256 claimTokensWorth =(FundsAllocated/contributionPercentage);
+        return claimTokensWorth;
+     }
+     // this particular function doesn't return contribution percentage with decimal precision,
+     //because solidity doesnt suppose floating point numbers
+   
+}
