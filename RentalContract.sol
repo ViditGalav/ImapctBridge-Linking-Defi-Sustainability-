a294ebbbad3da93c0b2467cda6f4fcbf9d96d130
vanshika4810 <124803849+vanshika4810@users.noreply.github.com>
2023-11-06 17:27:24 +0530

    Update RentalContract.sol

--- RentalContract.sol
+++ RentalContract.sol
@@ -6,7 +6,7 @@ contract RentalContract {
     
     struct Project {
         uint projectId;
-        address borrower;
+        address payable borrower;
         uint amount; //loan amount
         uint interestRate;
         uint totalRepaid;
@@ -37,14 +37,25 @@ contract RentalContract {
             project.isLoanApproved=true;        
             investorContributions[_projectId][msg.sender] = project.amount;
             investorProjects[msg.sender].push(_projectId);
+            amountTransfer(_projectId);
         }
+    }
+
+    // function to transfer amount.
+    function amountTransfer(uint _projectId) public payable {
+        Project memory project = projects[_projectId];
+        require(msg.sender != project.borrower, "Borrower can't transfer money to itself");
+        project.convenienceFee = (5*project.amount)/100;
+        require(msg.value == project.amount + project.convenienceFee, "Amount not sufficient");
+        project.borrower.transfer(project.amount);
     }
 
     // Borrower can repay the loan.
     function makeRepayment(uint _projectId, uint _amount) view public {
         Project memory project = projects[_projectId];
         require(!project.isClosed, "Loan is already closed"); 
-        require(msg.sender == project.borrower, "Only the borrower can make repayments");               
+        require(msg.sender == project.borrower, "Only the borrower can make repayments");  
+        project.convenienceFee = (5*project.amount)/100;             
         uint remainingAmount = project.convenienceFee + project.amount + (project.amount * project.interestRate / 100) - project.totalRepaid;
         require(_amount <= remainingAmount, "Repayment amount exceeds remaining balance");
         project.totalRepaid += _amount;
