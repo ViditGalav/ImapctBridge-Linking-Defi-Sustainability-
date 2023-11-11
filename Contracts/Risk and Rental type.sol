3830570f1b9f1fbb3601c3300e08bbae9ed3383a
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2023-11-11 17:56:05 +0530

    Update Risk and Rental type.sol
    Removed Stack too deep error but now encountering error:
    
    UnimplementedFeatureError: Copying of type struct ProjectRegistry.Risk memory[] memory to storage not yet supported.
    
    in Updating the risks of the project

--- Contracts/Risk and Rental type.sol
+++ Contracts/Risk and Rental type.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract ProjectRegistry {
     // Struct to represent project details
-    struct Project {
+    struct ProjectDetails {
         string name; // Name of the project
         string techUsed; // Technology used in the project
         uint256 fundsRequired; // Funds required for the project
@@ -18,21 +18,26 @@ contract ProjectRegistry {
         bool isSeekingLoan; // Flag to indicate if the project seeks a loan
         uint256 loanInterestRate; // Annual interest rate for a loan (if seeking a loan)
         uint256 loanDuration; // Loan duration in months (if seeking a loan)
-        Risk[] risks; // List of project risks
     }
 
     //Struct to represent Risks of the project
-        struct Risk {
+    struct Risk {
         string description;
         uint256 riskOccurrencePercentage; // Risk occurrence in percentage
         uint256 impact; // Impact of the risk in terms of loss
+    }
+
+    // Struct to represent a project
+    struct Project {
+        ProjectDetails details;
+        Risk[] risks; // List of project risks
     }
 
     // Struct to represent an investment in a project
     struct Investment {
         address investor; // Investor's Ethereum address
         uint256 amount; // Amount invested
-    }    
+    }
 
     // Mapping to store projects by project ID
     mapping(uint256 => Project) public projects;
@@ -44,113 +49,84 @@ contract ProjectRegistry {
     uint256 public projectCount;
 
     // Event to log the registration of a new project
-    event ProjectRegistered(
-        uint256 projectId,
-        string name,
-        string techUsed,
-        uint256 fundsRequired,
-        string socialDocumentIPFSHash,
-        string environmentalDocumentIPFSHash,
-        string financeDocumentIPFSHash,
-        string governanceDocumentIPFSHash,
-        string goals,
-        string impact,
-        uint256 tokensToIssue,
-        bool isSeekingInvestment,
-        bool isSeekingLoan,
-        uint256 loanInterestRate,
-        uint256 loanDuration
-    );
+    event ProjectRegistered(uint256 projectId, ProjectDetails details);
 
     // Event to log an investment in a project
-    event InvestmentReceived(uint256 projectId, address indexed investor, uint256 amount);
+    event InvestmentReceived(
+        uint256 projectId,
+        address indexed investor,
+        uint256 amount
+    );
 
-    event RiskAdded(uint256 projectId, string description, uint256 riskOccurrencePercentage, uint256 impact);
+    event RiskAdded(
+        uint256 projectId,
+        string description,
+        uint256 riskOccurrencePercentage,
+        uint256 impact
+    );
 
     // Function to register a new project
-    function registerProject(
-        string memory _name,
-        string memory _techUsed,
-        uint256 _fundsRequired,
-        string memory _socialDocumentIPFSHash,
-        string memory _environmentalDocumentIPFSHash,
-        string memory _financeDocumentIPFSHash,
-        string memory _governanceDocumentIPFSHash,
-        string memory _goals,
-        string memory _impact,
-        uint256 _tokensToIssue,
-        bool _isSeekingInvestment,
-        bool _isSeekingLoan,
-        uint256 _loanInterestRate,
-        uint256 _loanDuration
-    ) public {
+    function registerProject(ProjectDetails memory details) public {
         // Check that project name, technology used, and funds required are provided and valid
-        require(bytes(_name).length > 0, "Project name cannot be empty");
-        require(bytes(_techUsed).length > 0, "Technology used cannot be empty");
-        require(_fundsRequired > 0, "Funds required must be greater than 0");
+        require(bytes(details.name).length > 0, "Project name cannot be empty");
+        require(
+            bytes(details.techUsed).length > 0,
+            "Technology used cannot be empty"
+        );
+        require(
+            details.fundsRequired > 0,
+            "Funds required must be greater than 0"
+        );
 
         // Increment projectCount to generate a unique project ID
         projectCount++;
 
         // Create a new Project struct and store it in the projects mapping
-        projects[projectCount] = Project(
-            _name,
-            _techUsed,
-            _fundsRequired,
-            _socialDocumentIPFSHash,
-            _environmentalDocumentIPFSHash,
-            _financeDocumentIPFSHash,
-            _governanceDocumentIPFSHash,
-            _goals,
-            _impact,
-            _tokensToIssue,
-            _isSeekingInvestment,
-            _isSeekingLoan,
-            _loanInterestRate,
-            _loanDuration,
-            new Risk[](0) // Initialize the risks as an empty array
-        );
+        projects[projectCount] = Project(details, new Risk[](0));
 
         // Emit an event to log the registration of the new project
-        emit ProjectRegistered(
-            projectCount,
-            _name,
-            _techUsed,
-            _fundsRequired,
-            _socialDocumentIPFSHash,
-            _environmentalDocumentIPFSHash,
-            _financeDocumentIPFSHash,
-            _governanceDocumentIPFSHash,
-            _goals,
-            _impact,
-            _tokensToIssue,
-            _isSeekingInvestment,
-            _isSeekingLoan,
-            _loanInterestRate,
-            _loanDuration
-        );
+        emit ProjectRegistered(projectCount, details);
     }
 
-        function addRiskToProject(
+    function addRiskToProject(
         uint256 projectId,
         string memory description,
         uint256 riskOccurrencePercentage,
         uint256 impact
     ) public {
-        require(projectId > 0 && projectId <= projectCount, "Invalid project ID");
-        require(bytes(description).length > 0, "Risk description cannot be empty");
-        require(riskOccurrencePercentage >= 0 && riskOccurrencePercentage <= 100, "Risk occurrence percentage must be between 0 and 100");
+        require(
+            projectId > 0 && projectId <= projectCount,
+            "Invalid project ID"
+        );
+        require(
+            bytes(description).length > 0,
+            "Risk description cannot be empty"
+        );
+        require(
+            riskOccurrencePercentage >= 0 && riskOccurrencePercentage <= 100,
+            "Risk occurrence percentage must be between 0 and 100"
+        );
         require(impact >= 0, "Impact of risk must be non-negative");
 
         Project storage project = projects[projectId];
         project.risks.push(Risk(description, riskOccurrencePercentage, impact));
 
-        emit RiskAdded(projectId, description, riskOccurrencePercentage, impact);
+        emit RiskAdded(
+            projectId,
+            description,
+            riskOccurrencePercentage,
+            impact
+        );
     }
 
     // Function to calculate the cumulative risk percentage for a project
-    function calculateCumulativeRisk(uint256 projectId) public view returns (uint256) {
-        require(projectId > 0 && projectId <= projectCount, "Invalid project ID");
+    function calculateCumulativeRisk(
+        uint256 projectId
+    ) public view returns (uint256) {
+        require(
+            projectId > 0 && projectId <= projectCount,
+            "Invalid project ID"
+        );
 
         Project storage project = projects[projectId];
         uint256 totalRiskPercentage = 0;
@@ -163,131 +139,109 @@ contract ProjectRegistry {
     }
 
     // Function to get the details of a specific project by project ID
-    function getProjectDetails(uint256 projectId)
-        public
-        view
-        returns (
-            string memory,
-            string memory,
-            uint256,
-            string memory,
-            string memory,
-            string memory,
-            string memory,
-            string memory,
-            string memory,
-            uint256,
-            bool,  // Investment
-            bool,  //  Borrow Money
-            uint256,  // Borrow Percentage (if borrowing)
-            uint256,  // Borrow Duration (if borrowing)
-            Risk[] memory  // Array of risks
-        )
-    {
+    function getProjectDetails(
+        uint256 projectId
+    ) public view returns (ProjectDetails memory, Risk[] memory) {
         // Check if the project ID is valid
-        require(projectId > 0 && projectId <= projectCount, "Invalid project ID");
+        require(
+            projectId > 0 && projectId <= projectCount,
+            "Invalid project ID"
+        );
 
-    // Retrieve and return the details of the project
-    Project storage project = projects[projectId];
-    return (
-    project.name,
-    project.techUsed,
-    project.fundsRequired,
-    project.socialDocumentIPFSHash,
-    project.environmentalDocumentIPFSHash,
-    project.financeDocumentIPFSHash,
-    project.governanceDocumentIPFSHash,
-    project.goals,
-    project.impact,
-    project.tokensToIssue,
-    project.isSeekingInvestment,
-    project.isSeekingLoan,
-    project.loanInterestRate,
-    project.loanDuration,
-    project.risks
-);
+        // Retrieve and return the details of the project
+        Project storage project = projects[projectId];
+        return (project.details, project.risks);
     }
 
     // Function to update the details of an existing project
     function updateProjectDetails(
         uint256 projectId,
-        string memory _name,
-        string memory _techUsed,
-        uint256 _fundsRequired,
-        string memory _socialDocumentIPFSHash,
-        string memory _environmentalDocumentIPFSHash,
-        string memory _financeDocumentIPFSHash,
-        string memory _governanceDocumentIPFSHash,
-        string memory _goals,
-        string memory _impact,
-        uint256 _tokensToIssue,
-        bool _isSeekingInvestment,
-        bool _isSeekingLoan,
-        uint256 _borrowPercentage,
-        uint256 _borrowDuration,
-        Risk[] memory _risks
+        ProjectDetails memory details
     ) public {
         // Check if the project ID is valid
-        require(projectId > 0 && projectId <= projectCount, "Invalid project ID");
-
-        // Check that project name, technology used, and funds required are provided and valid
-        require(bytes(_name).length > 0, "Project name cannot be empty");
-        require(bytes(_techUsed).length > 0, "Technology used cannot be empty");
-        require(_fundsRequired > 0, "Funds required must be greater than 0");
+        require(
+            projectId > 0 && projectId <= projectCount,
+            "Invalid project ID"
+        );
 
         // Update the project details with the new values
-        projects[projectId] = Project(
-            _name,
-            _techUsed,
-            _fundsRequired,
-            _socialDocumentIPFSHash,
-            _environmentalDocumentIPFSHash,
-            _financeDocumentIPFSHash,
-            _governanceDocumentIPFSHash,
-            _goals,
-            _impact,
-            _tokensToIssue,
-            _isSeekingInvestment,
-            _isSeekingLoan,
-            _borrowPercentage,
-            _borrowDuration,
-            _risks
-        );
+        Project storage project = projects[projectId];
+        project.details = details;
     }
-    
+
+    // Function to update the details of an existing project
+    function updateProjectRisks(uint256 projectId, Risk[] memory risks) public {
+        // Check if the project ID is valid
+        require(
+            projectId > 0 && projectId <= projectCount,
+            "Invalid project ID"
+        );
+
+        // Update the risks for the project
+        Project storage project = projects[projectId];
+
+        // Clear existing risks
+        delete project.risks;
+
+        // Add new risks
+        for (uint256 i = 0; i < risks.length; i++) {
+            project.risks.push(
+                Risk(
+                    risks[i].description,
+                    risks[i].riskOccurrencePercentage,
+                    risks[i].impact
+                )
+            );
+        }
+    }
 
     // Function to allow investors to invest in a project
     function investInProject(uint256 projectId) external payable {
         // Check if the project ID is valid
-        require(projectId > 0 && projectId <= projectCount, "Invalid project ID");
+        require(
+            projectId > 0 && projectId <= projectCount,
+            "Invalid project ID"
+        );
         // Check that the investment amount is greater than 0
         require(msg.value > 0, "Investment amount must be greater than 0");
 
         // Store the investment details in the projectInvestments mapping
-        projectInvestments[projectId].push(Investment({investor: msg.sender, amount: msg.value}));
+        projectInvestments[projectId].push(
+            Investment({investor: msg.sender, amount: msg.value})
+        );
         // Emit an event to log the investment
         emit InvestmentReceived(projectId, msg.sender, msg.value);
     }
 
     // Function to get the list of investments for a specific project
-    function getProjectInvestments(uint256 projectId) public view returns (Investment[] memory) {
+    function getProjectInvestments(
+        uint256 projectId
+    ) public view returns (Investment[] memory) {
         // Check if the project ID is valid
-        require(projectId > 0 && projectId <= projectCount, "Invalid project ID");
+        require(
+            projectId > 0 && projectId <= projectCount,
+            "Invalid project ID"
+        );
         // Return the list of investments for the project
         return projectInvestments[projectId];
     }
 
     // Function to calculate the percentage of total pool contribution by an investor
-    function getInvestorContributionPercentage(uint256 projectId, address investor) public view returns (uint256) {
-        
+    function getInvestorContributionPercentage(
+        uint256 projectId,
+        address investor
+    ) public view returns (uint256) {
         // Check if the project ID is valid
-        require(projectId > 0 && projectId <= projectCount, "Invalid project ID");
-        
+        require(
+            projectId > 0 && projectId <= projectCount,
+            "Invalid project ID"
+        );
+
         Investment[] storage investments = projectInvestments[projectId];
-        
+
         uint256 totalInvestment = 0;
         uint256 investorContribution = 0;
-        
+
         for (uint256 i = 0; i < investments.length; i++) {
             Investment storage investment = investments[i];
             totalInvestment += investment.amount;
@@ -295,14 +249,13 @@ contract ProjectRegistry {
                 investorContribution += investment.amount;
             }
         }
-        
+
         if (totalInvestment == 0) {
             return 0; // Avoid division by zero
         }
-        
+
         return (investorContribution * 100) / totalInvestment;
     }
-    
 
     /*
     // Function to allow investors to withdraw their investments from a project
