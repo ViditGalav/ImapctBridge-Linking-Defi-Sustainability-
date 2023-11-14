c890cab6a58b5ddbe5a26679df8e651154a873ff
vanshika4810 <124803849+vanshika4810@users.noreply.github.com>
2023-11-14 11:07:01 +0530

    added riskPercentage function

--- Automated actions1.sol
+++ Automated actions1.sol
@@ -1,53 +1,59 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >= 0.7.0 < 0.9.0;
+pragma solidity ^0.8.0;
 
 contract RiskContract {
     uint public investedAmount;
     uint public payoutAmount; // Payout amount in the event of a risk event
-    uint public riskThreshold; // Project value*(Risk Threshold %/100)
+    uint public riskThreshold; // Project value * (Risk Threshold% / 100)
     uint public _riskScore;
 
     struct Project {
-        address projectId;
         uint riskThreshold;
+        uint riskPercentage;
     }
+
     mapping(address => Project) public projects;
 
-    constructor(
-        uint256 _investedAmount,
-        uint256 _riskThreshold
-    ) {     
-        payoutAmount = _investedAmount/2; //Since 50% of the invested amount is being returned.
-        riskThreshold = _riskThreshold;
-    }
-    
     struct Risk {
         string threat;
         uint riskProbability;
         uint riskImpact;
     }
 
-    // Array to store multiple risks
     Risk[] public risks; 
 
-    // Function to calculate Risk Score.
-    function calcRiskScore() public {
+    constructor(uint _investedAmount, uint256 _riskThreshold) {
+        investedAmount = _investedAmount;
+        payoutAmount = (_investedAmount / 2); // Since 50% of the invested amount is returned.
+        riskThreshold = _riskThreshold;
+    }
+    
+    function calcRiskScore() internal returns(uint) {
         _riskScore = 0; // Initialize the risk score
         
         for(uint i = 0; i < risks.length; i++) {
             // Risk Score = summation of (riskProbability * riskImpact) for all risks
             _riskScore += risks[i].riskProbability * risks[i].riskImpact;
         }
+        return _riskScore;
     }
 
-    function checkRiskAndTriggerPayout() external payable {
-        require(projects[msg.sender].projectId != address(0), "Project not registered.");
+    modifier projectRegistered(address _projectId) {
+        require(projects[_projectId].riskThreshold > 0, "Project not registered.");
+        _;
+    }
+
+    function checkRiskAndTriggerPayout() external projectRegistered(msg.sender) {
         calcRiskScore(); 
-        
         if (_riskScore >= riskThreshold) {
             // Transfer payoutAmount to the project
             payable(msg.sender).transfer(payoutAmount);
         }
     }
-}
 
+    function calcRiskPercentage(address _projectId) public view returns(uint){
+        Project memory project = projects[_projectId];
+        project.riskPercentage = (_riskScore/riskThreshold)*100;
+        return project.riskPercentage;
+    }
+}
